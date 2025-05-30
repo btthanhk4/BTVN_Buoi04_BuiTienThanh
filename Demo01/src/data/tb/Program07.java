eccf68a02d22d68ade84d09a18744ac2d216cbbe
btthanhk4 <thanhbeo2k4@gmail.com>
2025-05-30 08:12:48 +0700

    update

--- Demo01/src/data/tb/Program07.java
+++ Demo01/src/data/tb/Program07.java
@@ -7,7 +7,6 @@ public class Program07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Nhập số phần tử
         System.out.println("Nhập số phần tử của mảng: ");
         int n = scanner.nextInt();
 
@@ -18,17 +17,14 @@ public class Program07 {
 
         int[] array = new int[n];
 
-        // Nhập giá trị cho từng phần tử
         System.out.println("Nhập các phần tử:");
         for (int i = 0; i < n; i++) {
             array[i] = scanner.nextInt();
         }
 
-        // Tạo 2 danh sách để giữ thứ tự ban đầu
         ArrayList<Integer> evenList = new ArrayList<>();
         ArrayList<Integer> oddList = new ArrayList<>();
 
-        // Phân loại chẵn - lẻ
         for (int i = 0; i < n; i++) {
             if (array[i] % 2 == 0) {
                 evenList.add(array[i]);
@@ -37,7 +33,6 @@ public class Program07 {
             }
         }
 
-        // In mảng sau khi sắp xếp
         System.out.println("Mảng sau khi sắp xếp (chẵn trước, lẻ sau):");
         for (int i = 0; i < evenList.size(); i++) {
             System.out.print(evenList.get(i) + " ");
